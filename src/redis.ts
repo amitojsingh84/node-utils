d387f8630550e139ed8fa0cac585031b46124e46
Diwanshi <diwanshi.anand@affordplan.com>
2022-07-29 17:56:55 +0530

    changes

--- src/redis.ts
+++ src/redis.ts
@@ -1,86 +1,36 @@
 import { Logger }       from './logger'
 import { createClient } from 'redis'
-import { APError }      from './ap-error'
-import { Errors }        from './errors'
-import { REDIS_CONSTANTS } from './redis-constants'
 
-export class RedisOperations {
-  private _connected : boolean = false
-  private _client : any
-  private logger : Logger
 
-  constructor(config : any, logger : Logger) {
-    this._client = createClient({url : config.REDIS_URL})
-    this._client.on('error', (err : Error) => {
-      this.logger.error('Redis client error. %s', err)
-    })
-    this.logger = logger
+export class RedisUtility {
+
+  private connectionConfig : any
+  private logger           : Logger
+  private client           : any
+
+  constructor(logger : Logger, connectionConfig : any) {
+    this.connectionConfig = connectionConfig
+    this.logger           = logger
   }
   
+  async createRedisConnection() {
+    this.client = createClient(this.connectionConfig)
 
-  public async connect() {
-    if(!this._connected) {
-      this.logger.debug('Connecting to redis server.')
-      await this._client.connect()
-      this._connected = true
-    }
+    this.client.on('error', (err : any) => {
+      this.logger.error('Redis Client Error %s', err)
+    })
+
+    await this.client.connect()
   }
 
-  public async disconnect() {
-    if(this._connected) {
-      this.logger.debug('Disconnecting from redis server.')
-      await this._client.disconnect()
-      this._connected = false
-    }
+  async addValues(key: string, value : string) {
+    await this.client.set(key, value) 
   }
 
-  public async runCommand(command : string, ...params : any[]) {
-    if(!this._connected) {
-      this.logger.error('Redis client not connected. %s', this._connected)
-      throw new APError(Errors.name.REDIS_NOT_CONNECTED, Errors.message.REDIS_NOT_CONNECTED)
-    }
+  async getValues(key : string) {
+    const value = await this.client.get(key)
+    console.log(value)
 
-    this.logger.info('Sending redis command %s %s', command, JSON.stringify(params))
-    try {
-      return await this._client.sendCommand([command, ...params])
-    } catch(e) {
-      this.logger.error('Error in redis command. %s %s %s', command, JSON.stringify(params), e)
-      throw new APError(Errors.name.REDIS_ERROR, Errors.message.REDIS_ERROR)
-    }
   }
-
-  public async hget(mapName : string, key : string) {
-    return await this._runCommand(REDIS_CONSTANTS.COMMANDS.HGET, mapName, key)
-  }
-
-  public async hgetall(mapName : string) {
-    return await this._runCommand('HGETALL', mapName)
-  }
-
-  public async hset(mapName : string, key : string, data : string) {
-    return await this._runCommand(REDIS_CONSTANTS.COMMANDS.HSET, mapName, key, data)
-  }
-
-  public async hdel(mapName : string, key : string) {
-    return await this._runCommand(REDIS_CONSTANTS.COMMANDS.HDEL, mapName, key)
-  }
-
-/*------------------------------------------------------------------------------
-  PRIVATE METHODS
-------------------------------------------------------------------------------*/ 
-
-  private async _runCommand(command : string, ...params : any[]) {
-    this.logger.info('Running redis command %s %s', command, JSON.stringify(params))
-
-    try {
-      await this.connect()
-      return await this.runCommand(command, ...params)
-    } catch(e) {
-      this.logger.error('Error in redis command. %s %s %s', command, JSON.stringify(params), e)
-      throw new APError(Errors.name.REDIS_ERROR, Errors.message.REDIS_ERROR)
-    } finally {
-      await this.disconnect()
-    }
-  }
-
 }
+
